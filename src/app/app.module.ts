d756da18a77995b5dadc191049ba4c360574cfff
Volodymyr_Derunov <vderunov@gmail.com>
2019-10-23 11:28:02 +0300

    update packege.json and app.module.ts after update dev branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RegistrationShelterComponent } from './registration-shelter/registration-shelter.component';
 
 @NgModule({
   declarations: [AppComponent, RegistrationShelterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule,
+    AppRoutingModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule
