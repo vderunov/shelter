6681701ba3d81b1c1fe8383d02a54117b1427170
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-10-27 21:03:45 +0200

    fixed formatting issues

--- src/app/navbar/user-menu/user-menu.component.ts
+++ src/app/navbar/user-menu/user-menu.component.ts
@@ -1,30 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss']
 })
-export class UserMenuComponent implements OnInit {
+export class UserMenuComponent {
   constructor() { }
 
   public isLogged(): boolean {
     return Boolean(localStorage.getItem('user'));
   }
 
-  public getUser(): string | boolean {
-    if (this.isLogged()) {
+  public getUser(): string {
       return JSON.parse(localStorage.getItem('user'))[0]['email'];
-    } else {
-      return false;
-    }
   }
 
   public logOut(): void {
     localStorage.removeItem('user');
   }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/navbar/user-menu/user-menu.module.ts
+++ src/app/navbar/user-menu/user-menu.module.ts
@@ -3,18 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { UserMenuComponent } from './user-menu.component';
-import { AppRoutingModule } from '../../app-routing.module';
-
-
 
 @NgModule({
   declarations: [UserMenuComponent],
   imports: [
     CommonModule,
-    AppRoutingModule,
     MatMenuModule,
     MatIconModule,
-    AppRoutingModule
   ]
 })
+
 export class UserMenuModule { }
