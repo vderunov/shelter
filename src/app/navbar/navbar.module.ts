81b3f32f4a352f570d4028d19e2d1b3b904b6b09
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-10-27 03:55:14 +0300

    updated navbar

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { UserMenuComponent } from './user-menu/user-menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, UserMenuComponent],
   exports: [
     NavbarComponent
   ],
   imports: [
     CommonModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class NavbarModule { }
