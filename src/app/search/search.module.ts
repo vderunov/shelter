39b6852f8a5415af90105d1d607740cfb1b0493d
Nikolaenko M <nikomepf131@gmail.com>
2019-11-05 15:27:43 +0200

    reworked template of search component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-
 import { SearchComponent } from './search.component';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, MatInputModule, ReactiveFormsModule],
+  imports: [CommonModule, MatInputModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
   exports: [SearchComponent]
 })
 export class SearchModule {}
