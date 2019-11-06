8b1bfa3edcd2ae731d714c4d771e518a0088896b
nikom <nikomepf131@gmail.com>
2019-11-07 01:57:35 +0200

    reworked test for changed method getShelters

--- src/app/shelters/shelters-service/shelters.service.spec.ts
+++ src/app/shelters/shelters-service/shelters.service.spec.ts
@@ -8,7 +8,10 @@ describe('SheltersService', () => {
   let httpClientSpy: { get: jasmine.Spy };
   let configServiceSpy: { getConfig: jasmine.Spy };
   let configUrl: {
-    sheltersApi: string;
+    sheltersApi: string,
+    childrenApi: string,
+    representativesApi: string,
+    addressApi: string
   };
 
   beforeEach(() => {
@@ -16,7 +19,10 @@ describe('SheltersService', () => {
     configServiceSpy = jasmine.createSpyObj('configService', ['getConfig']);
     service = new SheltersService(<any>httpClientSpy, <any>configServiceSpy);
     configUrl = {
-      "sheltersApi": "https://familynetserver.azurewebsites.net/api/v1/childrenHouse"
+      "sheltersApi": "https://familynetserver.azurewebsites.net/api/v1/childrenHouse",
+      "childrenApi": "https://familynetserver.azurewebsites.net/api/v1/children/",
+      "representativesApi": "https://familynetserver.azurewebsites.net/api/v1/representatives/",
+      "addressApi": "https://familynetserver.azurewebsites.net/api/v1/address"
     }
     configServiceSpy.getConfig.and.returnValue(of(configUrl));
   });
@@ -25,15 +31,35 @@ describe('SheltersService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('Tests of getShelters method', () => {
+  describe('Method getShelters', () => {
+    let mockAnswerForApi: object;
     let mockShelters: Shelter[];
     beforeEach(() => {
-      mockShelters = [
-        { "id": 53, "name": "Ромашка", "adressID": 114, "rating": 11.0, "avatar": null, "locationID": null, "photoPath": "https://familynetserver.azurewebsites.net/3nf3pby0.jpg" },
-        { "id": 54, "name": "Ромашка", "adressID": 115, "rating": 12.0, "avatar": null, "locationID": null, "photoPath": "https://familynetserver.azurewebsites.net/4nf3pby0.jpg" }
-      ];
+      mockAnswerForApi = {
+        "https://familynetserver.azurewebsites.net/api/v1/childrenHouse": [
+          { "id": 53, "name": "Ромашка", "adressID": 114, "rating": 11.0 },
+          { "id": 54, "name": "Лопухи", "adressID": 115, "rating": 12.0 }
+        ],
+        "https://familynetserver.azurewebsites.net/api/v1/children/": [
+          { "id": 110, "name": "Глеб", "surname": "Левада", "patronymic": "Русланович", "childrenHouseID": 53, "childrenHouseName": "Ромашка" },
+          { "id": 111, "name": "Олег", "surname": "Курасов", "patronymic": "Михайлович", "childrenHouseID": 54, "childrenHouseName": "Лопухи" },
+        ],
+        "https://familynetserver.azurewebsites.net/api/v1/representatives/" : [
+          {"id":54,"name":"Олег","surname":"Петренко","patronymic":"Дмитреевич", "childrenHouseID":53 },
+          {"id":55,"name":"Марина","surname":"Кричич","patronymic":"Михайловна", "childrenHouseID":54 }
+        ],
+        "https://familynetserver.azurewebsites.net/api/v1/address": [
+          {"id":114,"country":"Украина","region":"Днепропетровская","city":"Днепр","street":"Артема","house":"250"},
+          {"id":115,"country":"Украина","region":"Днепропетровская","city":"Днепр","street":"Гагарина","house":"122"}
+        ]
+      };
+
+      mockShelters = [  
+        { id: 53, name: "Ромашка", adressID: 114, rating: 11.0, children: 1, representative: { id: 54, name: 'Олег', surname: 'Петренко', patronymic: 'Дмитреевич', childrenHouseID: 53 }, address: { id: 114, country: 'Украина', region: 'Днепропетровская', city: 'Днепр', street: 'Артема', house: '250' } },
+        { id: 54, name: 'Лопухи', adressID: 115, rating: 12, children: 1, representative: { id: 55, name: 'Марина', surname: 'Кричич', patronymic: 'Михайловна', childrenHouseID: 54 }, address: { id: 115, country: 'Украина', region: 'Днепропетровская', city: 'Днепр', street: 'Гагарина', house: '122' }}];
+
       configServiceSpy.getConfig.and.returnValue(of(configUrl));
-      httpClientSpy.get.and.returnValue(of(mockShelters));
+      httpClientSpy.get.and.callFake(api => of(mockAnswerForApi[api]));
     });
 
     it('should get all Shelters', (done: DoneFn) => {
@@ -43,7 +69,7 @@ describe('SheltersService', () => {
       });
     });
 
-    it('should get list of Shelters filtered by name "Ромашка"', (done: DoneFn) => {
+    it('should set search params to get request for shelter list', (done: DoneFn) => {
       const searchValue = "Ромашка";
       let params = new HttpParams();
       params = params.set("name", searchValue);
@@ -53,6 +79,6 @@ describe('SheltersService', () => {
         done();
       });
     });
-  })
+  });
 
 });
