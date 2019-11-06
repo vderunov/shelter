07d78e9b1650395bb42690f2991f5d6c6aba688d
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-11-06 11:45:44 +0200

    Update auth-state.service.ts

--- src/app/shared/services/state/auth-state.service.ts
+++ src/app/shared/services/state/auth-state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,25 @@ export class AuthStateService {
 
   public cleanAuthenticatedState() {
     this.setToken(null);
+  }
+
+  // private getUserInfo(): object {
+  //   return this.getState().pipe(
+  //     take(1),
+  //     map(tok => {
+  //       const userInfo = tok.match(/.*?\.(.*?)\..*/)[1];
+  //
+  //
+  //     })
+  //   )
+  //
+  // }
+
+  private base64decode(str: string): string {
+    return atob(str);
+  }
+
+  private getPayload(str: string): string {
+    return str.match(/.*?\.(.*?)\..*/)[1];
   }
 }
