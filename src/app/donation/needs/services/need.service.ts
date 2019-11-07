d041febd1ec1a786391fd49ed787bcaa4eebdf1f
nikom <nikomepf131@gmail.com>
2019-11-08 00:01:10 +0200

    created tests for method getQuests of questService service

--- src/app/donation/needs/services/need.service.ts
+++ src/app/donation/needs/services/need.service.ts
@@ -15,10 +15,6 @@ export class NeedService {
     private http: HttpClient,
     private configService: ConfigService) { }
 
-  // TODO. Nikolaienko Mikhail responsible for realization of search logic.
-  // Due to lack of search logic on backend, it is temporary realized on front-side.
-  // Nikolaienko Mikhail takes responsibility to negotiate with backers on matter of realization
-  // of search logic on beckend until next presentation.
   public getNeeds(paramObj: object = {}): Observable<Need[]> {
     let params = new HttpParams();
     Object.keys(paramObj).forEach((key: string) => params = params.set(key, paramObj[key]));
