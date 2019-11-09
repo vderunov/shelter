1d4c8b720b17dff296c49c53dfceb25dbbc39dc8
Volodymyr_Derunov <vderunov@gmail.com>
2019-11-09 23:15:30 +0200

    Rename description in tests

--- src/app/shelters/shelters-service/shelters.service.spec.ts
+++ src/app/shelters/shelters-service/shelters.service.spec.ts
@@ -20,11 +20,11 @@ describe('SheltersService', () => {
     configServiceSpy = jasmine.createSpyObj('configService', ['getConfig']);
     service = new SheltersService(httpClientSpy as any, configServiceSpy as any);
     configUrl = {
-      "sheltersApi": "https://familynetserver.azurewebsites.net/api/v1/childrenHouse",
-      "childrenApi": "https://familynetserver.azurewebsites.net/api/v1/children/",
-      "representativesApi": "https://familynetserver.azurewebsites.net/api/v1/representatives/",
-      "addressApi": "https://familynetserver.azurewebsites.net/api/v1/address"
-    }
+      'sheltersApi': 'https://familynetserver.azurewebsites.net/api/v1/childrenHouse',
+      'childrenApi': 'https://familynetserver.azurewebsites.net/api/v1/children/',
+      'representativesApi': 'https://familynetserver.azurewebsites.net/api/v1/representatives/',
+      'addressApi': 'https://familynetserver.azurewebsites.net/api/v1/address'
+    };
     configServiceSpy.getConfig.and.returnValue(of(configUrl));
   });
 
@@ -37,21 +37,21 @@ describe('SheltersService', () => {
     let mockShelters: Shelter[];
     beforeEach(() => {
       mockAnswerForApi = {
-        "https://familynetserver.azurewebsites.net/api/v1/childrenHouse": [
-          { "id": 53, "name": "Ромашка", "adressID": 114, "rating": 11.0 },
-          { "id": 54, "name": "Лопухи", "adressID": 115, "rating": 12.0 }
+        'https://familynetserver.azurewebsites.net/api/v1/childrenHouse': [
+          { id: 53, name: 'Ромашка', adressID: 114, rating: 11.0 },
+          { id: 54, name: 'Лопухи', adressID: 115, rating: 12.0 }
         ],
-        "https://familynetserver.azurewebsites.net/api/v1/children/": [
-          { "id": 110, "name": "Глеб", "surname": "Левада", "patronymic": "Русланович", "childrenHouseID": 53, "childrenHouseName": "Ромашка" },
-          { "id": 111, "name": "Олег", "surname": "Курасов", "patronymic": "Михайлович", "childrenHouseID": 54, "childrenHouseName": "Лопухи" },
+        'https://familynetserver.azurewebsites.net/api/v1/children/': [
+          { id: 110, name: 'Глеб', surname: 'Левада', patronymic: 'Русланович', childrenHouseID: 53, childrenHouseName: 'Ромашка' },
+          { id: 111, name: 'Олег', surname: 'Курасов', patronymic: 'Михайлович', childrenHouseID: 54, childrenHouseName: 'Лопухи' },
         ],
-        "https://familynetserver.azurewebsites.net/api/v1/representatives/" : [
-          {"id":54,"name":"Олег","surname":"Петренко","patronymic":"Дмитреевич", "childrenHouseID":53 },
-          {"id":55,"name":"Марина","surname":"Кричич","patronymic":"Михайловна", "childrenHouseID":54 }
+        'https://familynetserver.azurewebsites.net/api/v1/representatives/': [
+          { id: 54, name: 'Олег', surname: 'Петренко', patronymic: 'Дмитреевич', childrenHouseID: 53 },
+          { id: 55, name: 'Марина', surname: 'Кричич', patronymic: 'Михайловна', childrenHouseID: 54 }
         ],
-        "https://familynetserver.azurewebsites.net/api/v1/address": [
-          {"id":114,"country":"Украина","region":"Днепропетровская","city":"Днепр","street":"Артема","house":"250"},
-          {"id":115,"country":"Украина","region":"Днепропетровская","city":"Днепр","street":"Гагарина","house":"122"}
+        'https://familynetserver.azurewebsites.net/api/v1/address': [
+          { id: 114, country: 'Украина', region: 'Днепропетровская', city: 'Днепр', street: 'Артема', house: '250' },
+          { id: 115, country: 'Украина', region: 'Днепропетровская', city: 'Днепр', street: 'Гагарина', house: '122' }
         ]
       };
 
@@ -89,9 +89,8 @@ describe('SheltersService', () => {
     });
   });
 
-
-  describe('Method RegisterAddressShelter', () => {
-    it('create post request with address data', (done: DoneFn) => {
+  describe('Shelter registration post methods', () => {
+    it('should send a post request by sending addressData object', (done: DoneFn) => {
       const url = 'https://familynetserver.azurewebsites.net/api/v1/address';
       const addressData: AddressShelter = {
         country: 'USA',
@@ -113,11 +112,11 @@ describe('SheltersService', () => {
         expect(response).toEqual(mockAnswer);
         done();
       });
-  });
+    });
 
-    it('create post request with shelter data', (done: DoneFn) => {
+    it('should send a post request by sending shelterData object', (done: DoneFn) => {
       const url = 'https://familynetserver.azurewebsites.net/api/v1/childrenHouse';
-      const addressData: Shelter = {
+      const shelterData: Shelter = {
         name: 'SUM 41',
         rating: 33,
         adressID: 97,
@@ -135,7 +134,7 @@ describe('SheltersService', () => {
         photoPath: '127126e178dh1hd71'
       };
       httpClientSpy.post.and.returnValues(of(mockAnswer));
-      service.registrationShelter(url, addressData).subscribe((response: Shelter) => {
+      service.registrationShelter(url, shelterData).subscribe((response: Shelter) => {
         expect(response).toEqual(mockAnswer);
         done();
       });
