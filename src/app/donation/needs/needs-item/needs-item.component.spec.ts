9097656a36d22b2bda9c4b43820b3238cf5383b2
YanaSuprun <yana.ysuprun@gmail.com>
2019-11-12 10:13:16 +0200

    Added tests for needs item

--- src/app/donation/needs/needs-item/needs-item.component.spec.ts
+++ src/app/donation/needs/needs-item/needs-item.component.spec.ts
@@ -4,45 +4,49 @@ import { MatCardModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NeedsItemDetailsComponent } from '../needs-item-details/needs-item-details.component';
 import { By } from '@angular/platform-browser';
-import { Need } from '../models/need.interface';
+import { DebugElement } from '@angular/core';
 
-xdescribe('NeedsItemComponent', () => {
+describe('NeedsItemComponent', () => {
   let component: NeedsItemComponent;
   let fixture: ComponentFixture<NeedsItemComponent>;
+  let needDe: DebugElement;
+  let needEl: HTMLElement;
 
-  beforeEach(async(() => {
+  const mockNeed = {
+    id: 1,
+    city: 'Днепр',
+    orphanageID: 54,
+    donationItemID: 1,
+    status: 'Sended',
+    lastDateWhenStatusChanged: '2019-10-26T09:27:31.2431168',
+    orphanageName: 'NameS124',
+    itemName: 'Футболка',
+    itemDescription: 'Размер XS'
+  };
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ NeedsItemComponent, NeedsItemDetailsComponent ],
       imports: [ RouterTestingModule, MatCardModule ]
-    })
-    .compileComponents();
-  }));
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(NeedsItemComponent);
     component = fixture.componentInstance;
 
-    const needDe  = fixture.debugElement.query(By.css('.need'));
-    const needEl = needDe.nativeElement;
-
-    const expectedNeed = {
-      id: 1,
-      city: 'Днепр',
-      orphanageID: 54,
-      donationItemID: 1,
-      status: 'Sended',
-      lastDateWhenStatusChanged: '2019-10-26T09:27:31.2431168',
-      orphanageName: 'NameS124',
-      itemName: 'Футболка',
-      itemDescription: 'Размер XS'
-    };
-
-    component.need = expectedNeed;
+    needDe  = fixture.debugElement.query(By.css('.need'));
+    needEl = needDe.nativeElement;
+    component.need = mockNeed;
 
     fixture.detectChanges();
   });
 
-  xit('should create needs item', () => {
-    expect(component).toBeTruthy();
+  it('Expected displaing orphanage\'s name in uppercase', () => {
+    const expectedPipedName = mockNeed.orphanageName.toUpperCase();
+    expect(needEl.textContent).toContain(expectedPipedName);
+  });
+
+  it('Expected displaing date in format dd/mm/yyy', () => {
+    const expectedPipedDate = '26/10/2019';
+    expect(needEl.textContent).toContain(expectedPipedDate);
   });
 });
