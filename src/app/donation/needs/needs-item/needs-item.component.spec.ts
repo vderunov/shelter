5ee30e4b7dcc3d214681ed3d9c9461f9d7ba529e
YanaSuprun <yana.ysuprun@gmail.com>
2019-11-12 11:01:22 +0200

    Deleted unnecessary for unit testing part

--- src/app/donation/needs/needs-item/needs-item.component.spec.ts
+++ src/app/donation/needs/needs-item/needs-item.component.spec.ts
@@ -1,16 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NeedsItemComponent } from './needs-item.component';
 import { MatCardModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NeedsItemDetailsComponent } from '../needs-item-details/needs-item-details.component';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 describe('NeedsItemComponent', () => {
   let component: NeedsItemComponent;
   let fixture: ComponentFixture<NeedsItemComponent>;
-  let needDe: DebugElement;
-  let needEl: HTMLElement;
 
   const mockNeed = {
     id: 1,
@@ -32,21 +28,12 @@ describe('NeedsItemComponent', () => {
 
     fixture = TestBed.createComponent(NeedsItemComponent);
     component = fixture.componentInstance;
-
-    needDe  = fixture.debugElement.query(By.css('.need'));
-    needEl = needDe.nativeElement;
     component.need = mockNeed;
 
     fixture.detectChanges();
   });
 
-  it('Expected displaing orphanage\'s name in uppercase', () => {
-    const expectedPipedName = mockNeed.orphanageName.toUpperCase();
-    expect(needEl.textContent).toContain(expectedPipedName);
-  });
-
-  it('Expected displaing date in format dd/mm/yyy', () => {
-    const expectedPipedDate = '26/10/2019';
-    expect(needEl.textContent).toContain(expectedPipedDate);
+  it('should create component needs item', () => {
+    expect(component).toBeTruthy();
   });
 });
