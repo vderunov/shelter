259bd13dfd260677d9f2679ca2d480cf5e5300ea
nikom <nikomepf131@gmail.com>
2019-11-14 01:43:03 +0200

    changed template of search component in user list component

--- src/app/admin-users/admin-users.component.ts
+++ src/app/admin-users/admin-users.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-admin-users',
   templateUrl: './admin-users.component.html',
+  providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
   styleUrls: ['./admin-users.component.scss']
 })
 
-export class AdminUsersComponent {}
+export class AdminUsersComponent implements OnInit {
+  private location: Location;
+
+  constructor ( location: Location ) {
+    this.location = location;
+  }
+
+  ngOnInit(): void {
+    this.location.replaceState('users/helpers');
+    console.log(1);
+  }
+}
