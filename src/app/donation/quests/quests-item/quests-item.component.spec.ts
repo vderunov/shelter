910fedae152caeadd32728bf80e6ce3b7a906af3
YanaSuprun <yana.ysuprun@gmail.com>
2019-11-18 11:12:14 +0200

    Tests added

--- src/app/donation/quests/quests-item/quests-item.component.spec.ts
+++ src/app/donation/quests/quests-item/quests-item.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { QuestsItemComponent } from './quests-item.component';
+import { MatCardModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
 
-xdescribe('QuestsItemComponent', () => {
+describe('QuestsItemComponent', () => {
   let component: QuestsItemComponent;
   let fixture: ComponentFixture<QuestsItemComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ QuestsItemComponent ]
-    })
-    .compileComponents();
-  }));
+  const mockQuests = {
+    id: 1,
+    name: 'Delivery',
+    status: 'needed',
+    donationDescription: 'To deliver books',
+    orphanageName: 'NameS124',
+    orphanageID: 1,
+    volunteerID: '',
+    fromDate:'2019-10-26T09:27:31.2431168',
+    toDate: '2019-11-26T09:27:31.2431168'
+  };
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuestsItemComponent ],
+      imports: [ RouterTestingModule, MatCardModule ]
+    });
+
     fixture = TestBed.createComponent(QuestsItemComponent);
     component = fixture.componentInstance;
+    component.quest = mockQuests;
+
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create component quests item', () => {
     expect(component).toBeTruthy();
   });
 });
