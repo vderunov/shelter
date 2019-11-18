9a2562d8d3e92710520ef4d038f90c86e96e2e0d
Alona <dorozhynskaad@gmail.com>
2019-11-18 19:38:11 +0200

    I’m trying to resolve the conflicts

--- src/app/shared/permissions/models/permissions.enum.ts
+++ src/app/shared/permissions/models/permissions.enum.ts
@@ -5,15 +5,10 @@ export enum Permissions {
   shelterReset,
   shelterDelete,
   shelterUploadPhoto,
-<<<<<<< HEAD
-  // Users
-  showUserList,
-=======
   // Donations
   volunteerEdit,
   volunteerSave,
   volunteerDelete,
->>>>>>> fd58308f49fbb723dfadbee2f257723f74c97331
   // Auction
   auctionShowPurchase
 }
