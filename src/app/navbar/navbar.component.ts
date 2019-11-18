ad7dd94e9ea86cdd1a814b22c80006744cd8e286
Volodymyr Shovkuta <47761397+vshovkuta@users.noreply.github.com>
2019-11-18 11:16:03 +0200

    Removed unused module

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
   constructor() {}
 }
